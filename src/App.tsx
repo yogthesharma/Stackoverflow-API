006914d5d7f9b8cd38a3fd6d754aec2189f38db2
Yog Sharma <yogthesharma@gmail.com>
2021-06-22 13:03:43 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col h-full items-center justify-center text-white bg-gradient-to-br from-gray-600 via-teal-700 to-gray-800">
+      This is tailwind and cra with typescript.
+    </div>
+  );
+}
+
+export default App;
